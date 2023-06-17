bd326b1b4bd44cdd772190316e722bcbcf08ece9
wolfpackjack <44581587+wolfpackjack@users.noreply.github.com>
2023-06-17 12:04:47 -0400

    setting up repo

---
+++ ue5testing/Intermediate/Source/test.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class test : ModuleRules
+{
+	public test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
